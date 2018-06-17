e09ae5bc6da85534e0b6f659d8dd2438adc32882
!!!XD <mark.koshkin@stonybrook.edu>
2018-06-17 18:04:50 -0400

    55

---
+++ include/filters.h
@@ -0,0 +1,8 @@
+#include <getPixels.h>
+#include <math.h>
+
+
+void de(pixel* in, pixel* out, int* args);
+void que(pixel* in, pixel* out, int* args);
+
+Image* filter(Image* input, int startValue); 
